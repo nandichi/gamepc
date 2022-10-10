f8004e95e77956bbdea68eed7792c087484740d8
Andichi, Naoufal - Student Rijn IJssel <00313992@rijnijssel.nl>
2022-10-10 10:04:46 +0200

    Changes by david

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -1,6 +1,7 @@
 <div class="wrapper">
     <ul class="nav-area">
-        <li><a href=index.php?page=login>login</a> </li>
+        <li><a href=index.php?page=homepage>home</a> </li>
+        <li><a href=index.php?page=pcbuild>pc-bouwen</a> </li>
         <li><a href=index.php?page=registreer>registreer</a> </li>
         <li><a href=index.php?page=pcbuild>pc-bouwen</a> </li>
         <li><a href=index.php?page=homepage>home</a> </li>
