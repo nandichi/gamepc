8293cbf122a3266774430ca9a94c562df2fa583d
Andichi, Naoufal - Student Rijn IJssel <00313992@rijnijssel.nl>
2022-10-10 11:23:35 +0200

    Admin rights + navbar changes

---
+++ includes/ADMIN.inc.php
@@ -0,0 +1,4 @@
+<div style="color: #ffffff" class="content">
+    <h1>ADMIN PAGE</h1>
+    <p>hier kan je alles aanpassen als admin</p>
+</div>
--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -3,11 +3,22 @@
         <li><a href=index.php?page=homepage>home</a> </li>
         <li><a href=index.php?page=pcbuild>pc-bouwen</a> </li>
         <li><a href=index.php?page=registreer>registreer</a> </li>
+
+
         <?php
         if (in_array("URID", $_SESSION)) {
             echo "<li><a href=index.php?page=logout>logout</a> </li>";
         } else {
             echo "<li><a href=index.php?page=login>login</a> </li>";
+        }
+
+        if (isset($_SESSION['ingelogd'])) {
+            echo '<li><a href="index.php?page=ADMIN">admin</a></li>';
+            echo "<li><a href=index.php?page=logout>logout</a> </li>";
+        }
+
+        elseif (isset($_SESSION['ingelogd1'])) {
+            echo "<li><a href=index.php?page=logout>logout</a> </li>";
         }
         ?>
     </ul>
